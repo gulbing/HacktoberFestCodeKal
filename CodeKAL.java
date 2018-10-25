9b22f28fa4ea929c2fe3ebc871abaa1845661aeb
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2018-10-25 18:18:56 +0300

    Create CodeKAL.java

---
+++ CodeKAL.java
@@ -0,0 +1,13 @@
+public class CodeKal {
+
+	public static void main(String[] args) {
+
+		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretleri Java'nın bir
+		// parçası. Çevirirken silmeyiniz.
+
+		System.out.println("CodeKAL'ı çok seviyorum.");
+		System.out.println("CodeKAL'da olduğum için çok mutluyum.");
+
+	}
+
+}
