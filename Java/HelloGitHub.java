99cf05c20006b401e9965d2182720c7a27b2ae2a
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2018-10-25 21:45:12 +0300

    Create HelloGitHub.java

---
+++ Java/HelloGitHub.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+	    System.out.println("Hello, World!");
+	    
+	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
+
+	}
+
+}
